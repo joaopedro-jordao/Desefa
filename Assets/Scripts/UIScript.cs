14527dfb451ab9431045370529a1c3a8b28ed697
pedrohrarruda <phra@cin.ufpe.br>
2022-05-12 22:10:05 -0300

    multiplayer pronto??

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIScript : MonoBehaviour
 {
 
     public static UIScript Instance{ set; get; }
+
+    public Server server;
+    public Client client;
     
     [SerializeField] private Animator menuAnimator;
+    [SerializeField] private TMP_InputField addressInput;
 
     private void Awake(){
         Instance = this;
     }
 
     public void OnHostGameButton(){
-        Debug.Log("OnHost");
+        server.Init(8007);
+        client.Init("127.0.0.1", 8007);
         menuAnimator.SetTrigger("HostMenuAnimation");
     }
 
@@ -23,6 +29,8 @@ public class UIScript : MonoBehaviour
     }
 
     public void OnHostBackButton(){
+        server.Shutdown();
+        client.Shutdown();
         menuAnimator.SetTrigger("MenuAnimation");
     }
 
@@ -31,6 +39,7 @@ public class UIScript : MonoBehaviour
     }
 
     public void OnEnterGameConnectButton(){
+        client.Init(addressInput.text, 8007);
         menuAnimator.SetTrigger("OnEnterGameConnectButton");//$$
     }
 
